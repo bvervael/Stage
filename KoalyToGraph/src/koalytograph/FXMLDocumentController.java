a5c3773ffb8fcb413482c5771d21496facdd7087
bvervael <bert.vervaele.v7335@student.hogent.be>
2017-02-23 09:42:25 +0100

    Editable cells and delete button

--- KoalyToGraph/src/koalytograph/FXMLDocumentController.java
+++ KoalyToGraph/src/koalytograph/FXMLDocumentController.java
@@ -5,12 +5,14 @@
  */
 package koalytograph;
 
-import java.awt.Checkbox;
 import java.io.*;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -18,6 +20,7 @@ import java.util.TreeMap;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
@@ -26,6 +29,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.usermodel.Cell;
@@ -42,7 +46,8 @@ public class FXMLDocumentController implements Initializable {
     private static String[][] importData;
     private LocalDate[] datum;
     private Map<LocalDate,Integer> months;
-    private double width=854,height=456;
+    private Map<String,XYChart.Series> graphs=new HashMap<>();
+    private List<LocalDate> allMonths= new ArrayList();
     
     @FXML
     private Button button;
@@ -57,49 +62,69 @@ public class FXMLDocumentController implements Initializable {
     private LineChart graph;
     
     @FXML
-    private CheckBox checkbox;
-    
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         String word=textField.getText().toLowerCase();
         textField.setText("");
         listView.setItems(list);
-        wordCount(word,checkbox.isSelected());
+        wordCount(word,true);
     }
     
-    public void wordCount(String word, Boolean exclusive){
+   @FXML
+   private void actionDelete(ActionEvent event) {
+        int selectedIdx = listView.getSelectionModel().getSelectedIndex();
+        if (selectedIdx != -1) {
+            String word = (String) listView.getSelectionModel().getSelectedItem();
+            listView.getItems().remove(selectedIdx);
+            graph.getData().remove(graphs.get(word));
+        }
+    }
+    
+    public void wordCount(String word,Boolean addToList){
         int counter=0;
         XYChart.Series series = new XYChart.Series();
         series.setName(word);
-        
         months = new TreeMap<>();
+        for(LocalDate date : allMonths){
+            months.put(date, 0);
+        }
         for(int i = 24; i <importData[0].length; i++){
             if(importData[7][i]!=null&&datum[i]!=null){
                 if(importData[7][i].toLowerCase().contains(word)){
-                    if(months.containsKey(datum[i])){
+                    //if(months.containsKey(datum[i])){
                         months.put(datum[i],months.get(datum[i])+1);
                         counter ++;
-                    } else {
-                        months.put(datum[i],1);
-                        counter ++;
-                    }
                 }
             }
         }
-        if (!months.isEmpty()) {
-           list.add(word);
+        if (counter != 0) {
+            if(addToList){
+                list.add(word);
+            }
             months.keySet().forEach((it) -> {
-                //System.out.println(it.toString() + ": " + months.get(it));
                 series.getData().add(new XYChart.Data(it.toString().substring(0, 7), months.get(it)));
             });
 
             graph.getData().add(series);
+            graphs.put(word, series);
         }
         System.out.println(word + " komt in " + counter + " titels voor.");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb){
+        listView.setEditable(true);
+        listView.setCellFactory(TextFieldListCell.forListView());
+        listView.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>() {
+			@Override
+			public void handle(ListView.EditEvent<String> t) {
+                                String word = (String) listView.getItems().set(t.getIndex(), t.getNewValue().toLowerCase());
+                                graph.getData().remove(graphs.get(word));
+				listView.getItems().set(t.getIndex(), t.getNewValue());
+                                wordCount((String)listView.getItems().set(t.getIndex(), t.getNewValue().toLowerCase()),false);
+			}
+						
+		});
+        
         int rowNum=0,colNum=0;
         try ( //Create Workbook from Existing File
             InputStream fileIn = new FileInputStream("C:\\tmp\\ticket.xls")) {
@@ -146,6 +171,9 @@ public class FXMLDocumentController implements Initializable {
             if(importData[13][u]!=null){
                 LocalDate date = LocalDate.parse(importData[13][u].substring(0,7)+"-01",format);
                 datum[u]=date;
+                if(!allMonths.contains(date)){
+                    allMonths.add(date);
+                }
             }else{
                 datum[u]=null;
             }
